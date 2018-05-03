c0e985e1a258712035190477164c640e230a6d4e
qhan1028 <michael.841028@gmail.com>
2018-05-03 11:53:55 +0800

    update download.sh

--- pj1/download.sh
+++ pj1/download.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+
+mkdir data
 wget http://nlg.csie.ntu.edu.tw/nlpresource/NTUSD-Fin/NTUSD-Fin.zip
-unzip -u NTUSD-Fin.zip -d data
-rm NTUSD-Fin.zip
+unzip -u NTUSD-Fin.zip
+mv NTUSD-Fin/*.json data/
+rm -rf NTUSD-Fin.zip NTUSD-Fin
